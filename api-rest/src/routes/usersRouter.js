8ec2dd87032d37a0d3e649815648912d4fc7a13c
italo-rodrigues1 <italosport110@hotmal.com>
2021-07-08 14:49:22 -0300

    routas

--- api-rest/src/routes/usersRouter.js
+++ api-rest/src/routes/usersRouter.js
@@ -5,13 +5,22 @@ const routes = Router();
 
 routes.get('/livros', produtosController.getAll);
 routes.get('/livros/:id', produtosController.getOneId);
+routes.get('/livros/autor/:id', produtosController.getAutor);
+routes.get('/livros/editora/:id', produtosController.getEditora);
+routes.get('/livros/idade/:idade', produtosController.getIdade);
 
+routes.post('/livros'.produtosController.create)
+routes.post('/livros/autor'.produtosController.create)
+routes.post('/livros/editora'.produtosController.create)
 
-/*
+routes.put('/livros/:id'.produtosController.update)
+routes.put('/livros/autor/:id'.produtosController.update)
+routes.put('/livros/editora/:id'.produtosController.update)
 
-routes.post('/produtos', produtosController.create);
-routes.put('/produtos/:id', produtosController.update);
-*/
+routes.delete('/livros/:id'.produtosController.delete)
+routes.delete('/livros/autor/:id'.produtosController.delete)
+routes.delete('/livros/editora/:id'.produtosController.delete)
+
 
 
 module.exports = routes;
