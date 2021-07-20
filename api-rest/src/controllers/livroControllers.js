ea38a25d3affdc59cfbd2501f8c09cd478e7f3a9
farelanders <marcostravagin@outlook.com>
2021-07-20 14:37:36 -0300

    feito pull/get/delete de livrocontroller

--- api-rest/src/controllers/livroControllers.js
+++ api-rest/src/controllers/livroControllers.js
@@ -1,36 +1,23 @@
-// exports.getOneId = (req, res, next) => {
-
-//   const {id} = req.params;
-  
-//   knex.where({id : id}).table("livros").then(data => {
-//     return res.status(201).json(data)
-//   }).catch(err =>{
-//     return res.status(400).json(err)
-//   });
-
-// }
-
-
 const knex = require('../database/index');
 
 
 exports.getAll = (req, res) => {
 
-  knex('livros').then((produtos)=>{
-      return res.status(200).json(produtos);
+  knex('livros').then((livros)=>{
+      return res.status(200).json(livros);
   })   
   
 }
 
 exports.getFaixaEtaria = (req, res) => {
-  knex('livros').then((produtos)=>{
+  knex('livros').then((livros)=>{
       
       let faixaEtaria = req.params.faixaetaria.split("-")
       let retorno = []
-      for ( i = 0 ; i < produtos.length ; i++){
-        let faixaEtariaCompara = produtos[i].idade.split("-")
+      for ( i = 0 ; i < livros.length ; i++){
+        let faixaEtariaCompara = livros[i].idade.split("-")
         if (faixaEtaria[0] >= faixaEtariaCompara[0] && faixaEtaria[0] <= faixaEtariaCompara[1] || faixaEtaria[1] >= faixaEtariaCompara[0] && faixaEtaria[1] <= faixaEtariaCompara[1])
-        retorno.push(produtos[i])         
+        retorno.push(livros[i])         
       }
       if (retorno[0] == null){
         return res.status(400).json({message: "Nenhum livro encontrado para esta faixa etária"});
@@ -40,8 +27,8 @@ exports.getFaixaEtaria = (req, res) => {
 }
 
 exports.delete = (req, res) => {
-  knex('livros').where('id', req.params.id).del().then((produto)=>{
-      if (produto == 0){
+  knex('livros').where('id', req.params.id).del().then((livro)=>{
+      if (livro == 0){
         return res.status(400).json({message: "Livro não encontrado em nosso banco de dados!"});
       }
       return res.status(200).json({message: "Livro removido com sucesso"});
@@ -50,8 +37,8 @@ exports.delete = (req, res) => {
 
 exports.update = (req, res) => {
   const {id, titulo, descricao, idade, destaque, idAutor, idCategoria, idEditora,	imagem } = req.body
-  knex('livros').where({id: id}).update({titulo: titulo, descricao: descricao, idade: idade, destaque: destaque, idAutor: idAutor, idCategoria: idCategoria, idEditora: idEditora, imagem: imagem}).then((produto)=>{
-      if (produto == 0){
+  knex('livros').where({id: id}).update({titulo: titulo, descricao: descricao, idade: idade, destaque: destaque, idAutor: idAutor, idCategoria: idCategoria, idEditora: idEditora, imagem: imagem}).then((livro)=>{
+      if (livro == 0){
         return res.status(400).json({message: "Livro não encontrado em nosso banco de dados!"});
       }
       return res.status(200).json({message: "Livro alterado com sucesso"});
--- api-rest/src/routes/usersRouter.js
+++ api-rest/src/routes/usersRouter.js
@@ -3,30 +3,24 @@
 // routes.get('/livros/editora/:id', produtosController.getEditora);
 
 const Router = require('express'); 
-const livroControllers = require('../controllers/livroControllers') ;
+const livroController = require('../controllers/livroControllers') ;
 const usuarioController = require('../controllers/usuarioControllers');
 const routes = Router(); 
 
 
-routes.get('/livros', livroControllers.getAll);//PRONTO
-routes.get('/livros/faixaetaria/:faixaetaria', livroControllers.getFaixaEtaria);//PRONTO
+routes.get('/livros', livroController.getAll);//PRONTO
+routes.get('/livros/faixaetaria/:faixaetaria', livroController.getFaixaEtaria);//PRONTO
 
 routes.get('/usuario', usuarioController.getUsuario);//PRONTO
 
 
-// routes.post('/livros'.produtosController.create);
-// routes.post('/livros/autor'.produtosController.create);
-// routes.post('/livros/editora'.produtosController.create);
-// routes.post('/usuario'.produtosController.create);
 
-routes.put('/livros', livroControllers.update);//PRONTO
-// routes.put('/livros/autor/:id'.produtosController.update);
-// routes.put('/livros/editora/:id'.produtosController.update);
 
-routes.delete('/livros/:id', livroControllers.delete);//PRONTO
-// routes.delete('/livros/autor/:id'.produtosController.delete);
-// routes.delete('/livros/editora/:id'.produtosController.delete);
-// routes.delete('/usuario'.produtosController.delete);
+routes.put('/livros', livroController.update);//PRONTO
+
+
+routes.delete('/livros/:id', livroController.delete);//PRONTO
+
 
 
 module.exports = routes;
