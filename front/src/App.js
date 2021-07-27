8380b2eba0283a380a87adcd8776f83c7a1a905d
farelanders <marcostravagin@outlook.com>
2021-07-27 12:01:34 -0300

    começo da estrutura do front

--- front/src/App.js
+++ front/src/App.js
@@ -1,22 +1,20 @@
-
+import { Link } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="logo">
+          <img src='#'></img>
+        </div>
+        <Link to="#"></Link>
+        <Link to="#"></Link>
       </header>
+      <div></div>
+      <section></section>
+      <section></section>
+      <section></section>
+      <section></section>
     </div>
   );
 }
