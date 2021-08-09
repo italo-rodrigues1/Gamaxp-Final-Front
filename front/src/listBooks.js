1c9b8f3c462a96140341e7bea11a992a96aebc85
italo-rodrigues1 <italosport110@hotmal.com>
2021-08-08 22:30:27 -0300

    nada

--- front/src/listBooks.js
+++ front/src/listBooks.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import Backtotop from './components/buttonBackToTop'
 import FooterContato from './components/footer';
 import HeaderTopo from './components/header';
 import FiltroBtn from './components/filtroBtn';
 import ModalLivro from './components/modal';
 import Pagination from './components/paginacao';
-import './styles/listBooks.css'
+import './styles/listBooks.css';
+import axios from 'axios';
 
-function listBooks() {
+const ListBooks = () => {
   
-  const api = 'https://kitsu.io/api/edge/';
+  // const api = 'https://kitsu.io/api/edge/anime';
+
+  const [livros, setItens] = useState([])
+  
+  useEffect(()=>{
+    const fetchData = async () =>{
+      const result = axios.get('https://kitsu.io/api/edge/anime')
+      .then(response => response.json())
+      .then(data => data)
+
+      setItens(result)
+
+    }
+    fetchData()
+  },[])
+
+  console.log(livros)
 
   return (
     <div>
@@ -20,10 +37,21 @@ function listBooks() {
         </div>
       </div>
       <FiltroBtn/>
+
+      {livros.data && (
+        <ul>
+        {livros.data.map(item => {
+          return <div className="item"  key={item.id}>
+            <img src={item.anime.attributes.posterImage.small} width='40%'/> 
+          </div>
+        })} 
+        </ul>
+      )}
+
       <FooterContato />
       <Backtotop />
     </div>
   )
 }
 
-export default listBooks
+export default ListBooks;
